ecfeab6ff13b4c095ba36f106ce5bbad4abc5c43
Abdalla Ahmed <101481715+abdallavhh@users.noreply.github.com>
2024-12-15 03:19:44 +0200

    Update IV_Pic.py

--- IV_Pic.py
+++ IV_Pic.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('img5.jpg')
+image = cv2.imread('img4.jpg')
 
 def contra_harmonic_mean_filter(image, size, Q):
     image = image.astype(np.float32)
